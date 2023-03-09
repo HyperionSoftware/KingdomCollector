5ad38f2afcbebeab8e54d31d065891cf1879e053
Carlos <carlosmg.12@hotmail.es>
2023-03-09 18:04:16 +0100

    Depuración de código

--- app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/MainActivity.java
+++ app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/MainActivity.java
@@ -14,8 +14,6 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_aboutus;
-    ImageButton play, btn_settings;
     SoundPool sp;
     private int sonido_reproduccion;
 
@@ -26,39 +24,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //Button Settings.
-        btn_settings = (ImageButton) findViewById(R.id.btn_settings);
-        btn_settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivities(new Intent[]{new Intent(MainActivity.this, TermsAndConditions.class)});
-            }
-        });
-
+        findViewById(R.id.btn_settings).setOnClickListener(view -> setBtn_aboutus());
 
         //Button AboutUs.
-        btn_aboutus = (Button) findViewById(R.id.btn_aboutUs);
-        btn_aboutus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivities(new Intent[]{new Intent(MainActivity.this, AboutUs.class)});
-            }
-        });
+        findViewById(R.id.btn_aboutUs).setOnClickListener(view -> setBtn_settings());
 
-        // Music
-        play = (ImageButton) findViewById(R.id.btn_start_button);
+        //Music
+        findViewById(R.id.btn_start_button).setOnClickListener(view -> setSonido_reproduccion());
+
+
         //sp = new SoundPool(1, AudioManager.STREAM_MUSIC,1);
-        //sonido_reproduccion = sp.load(this,R.raw.check_it_out_now, 1);
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivities(new Intent[]{new Intent(MainActivity.this, activity_tablero.class)});
-                sp = new SoundPool(1, AudioManager.STREAM_MUSIC,1);
-                sonido_reproduccion = sp.load("res/raw/check_it_out_now.mp3",1);
-               //sonido_reproduccion = sp.load(this,R.raw.check_it_out_now,1);
-            }
-        });
+        //sonido_reproduccion = sp.load(this, R.raw.check_it_out_now,1);
+
+
 
     }
     // Audio para reproducción corta.
@@ -71,6 +50,24 @@ public class MainActivity extends AppCompatActivity {
     public void AudioMediaPlayer(View view){
         MediaPlayer mp = MediaPlayer.create(this,R.raw.check_it_out_now);
         mp.start();
+    }
+
+    public void setBtn_aboutus(){
+        Intent intent = new Intent(this, AboutUs.class);
+        startActivity(intent);
+    }
+
+    public void setBtn_settings(){
+        Intent intent = new Intent(this, TermsAndConditions.class);
+        startActivity(intent);
+    }
+    public void setSonido_reproduccion(){
+        Intent intent = new Intent(this, activity_tablero.class);
+        startActivity(intent);
+        sp = new SoundPool(1, AudioManager.STREAM_MUSIC,1);
+        sonido_reproduccion = sp.load(this, R.raw.check_it_out_now,1);
+       // sonido_reproduccion = sp.load("res/raw/check_it_out_now.mp3",1);
+
     }
 
 }
