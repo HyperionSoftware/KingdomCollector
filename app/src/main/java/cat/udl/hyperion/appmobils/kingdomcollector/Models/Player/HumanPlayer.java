589c3ab3d8a4c191eef5b22a63a4f5e45d26e9c4
JoanBonell <joanbonellruiz@gmail.com>
2023-03-19 21:17:06 +0100

    Adding documentation, vaig a sopar

--- app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/Models/Player/HumanPlayer.java
+++ app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/Models/Player/HumanPlayer.java
@@ -4,17 +4,33 @@ import cat.udl.hyperion.appmobils.kingdomcollector.Models.Board;
 import cat.udl.hyperion.appmobils.kingdomcollector.Models.Card;
 
 /**
- * Esta clase representa un jugador humano.
- * */
+ * Esta clase representa un jugador humano en un juego de cartas.
+ */
 public class HumanPlayer extends Player {
+
+    /**
+     * Constructor de la clase HumanPlayer.
+     *
+     * @param name El nombre del jugador humano.
+     */
     public HumanPlayer(String name) {
-        super(name);
+        super(name); // Llama al constructor de la clase padre (Player) para inicializar el nombre.
     }
 
+    /**
+     * Método para que el jugador humano seleccione una carta para jugar.
+     *
+     * @param board El tablero actual del juego.
+     * @return La carta seleccionada por el jugador humano, o null si no se ha seleccionado ninguna.
+     */
     @Override
     public Card playCard(Board board) {
-        // TODO: Implementar la lógica de selección de carta para el jugador humano
+        // TODO: Implementar la lógica de selección de carta para el jugador humano.
+        // En este método se debe programar la lógica que permita al jugador humano seleccionar
+        // una carta para jugar. Una vez que se haya seleccionado la carta, se debe devolver
+        // un objeto de tipo Card con la carta seleccionada. Si el jugador humano no selecciona
+        // ninguna carta, se debe devolver null.
         Card card = null;
-        return card;
+        return card; // Devuelve la carta seleccionada o null si no se ha seleccionado ninguna.
     }
 }
