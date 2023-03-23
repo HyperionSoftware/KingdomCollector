543b824d5b235c12f71812123af74eb181b37230
JoanBonell <joanbonellruiz@gmail.com>
2023-03-23 12:02:54 +0100

    T3.2: Save game information after a user finishes a match. //TODO: implement when finishes the match. This works to add the information to database, but the function is now never called.

--- app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/Models/Game.java
+++ app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/Models/Game.java
@@ -1,13 +1,25 @@
 package cat.udl.hyperion.appmobils.kingdomcollector.Models;
 
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import cat.udl.hyperion.appmobils.kingdomcollector.Models.Board;
 import cat.udl.hyperion.appmobils.kingdomcollector.Models.Card;
 import cat.udl.hyperion.appmobils.kingdomcollector.Models.Player.Player;
+import cat.udl.hyperion.appmobils.kingdomcollector.views.GameView;
+import cat.udl.hyperion.appmobils.kingdomcollector.views.LoginActivity;
 
 /**
  * Esta clase representa el juego Kingdom Collector.
  * */
 public class Game {
+    protected String myClassTag = this.getClass().getSimpleName();
     private Player player1;
     private Player player2;
     private Board board;
@@ -77,5 +89,27 @@ public class Game {
         player2.resetCards();
         currentPlayer = player1;
         isGameOver = false;
+    }
+    /**
+     * Guarda la información de la partida cuando se termina.
+     * @param userId
+     * @param score
+     */
+    public void saveGameData(String userId, int score) {
+        DatabaseReference gameDataRef = FirebaseDatabase.getInstance().getReference("game_data").child(userId);
+
+        Map<String, Object> gameData = new HashMap<>();
+        gameData.put("userId", userId);
+        gameData.put("score", score);
+
+        gameDataRef.push().setValue(gameData)
+                .addOnSuccessListener(aVoid -> {
+                    // Información del juego guardada exitosamente
+                    Log.d(myClassTag, "Guardado el ganador exitosamente en firebase database.");
+                })
+                .addOnFailureListener(e -> {
+                    // Error al guardar la información del juego
+                    Log.d(myClassTag, "No se ha guardado el ganador en firsebase database");
+                });
     }
 }
