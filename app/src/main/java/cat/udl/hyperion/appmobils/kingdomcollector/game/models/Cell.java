5e70c75defdbf389843e3288880c23d82bb9f25c
RickyBP <ricardbope@gmail.com>
2023-05-11 22:34:35 +0200

    Logcats

--- app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/game/models/Cell.java
+++ app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/game/models/Cell.java
@@ -1,6 +1,8 @@
 package cat.udl.hyperion.appmobils.kingdomcollector.game.models;
 
 import androidx.databinding.ObservableField;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Cell {
     private ObservableField<Card> card;
@@ -35,5 +37,30 @@ public class Cell {
 
     public void notifyPropertyChanged(int fieldId) {
         card.notifyChange();
+    }
+
+    // Método para convertir la celda a un mapa
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        // Almacenamos la fila y la columna de la celda
+        result.put("row", this.row);
+        result.put("col", this.col);
+        // Si hay una carta en la celda, la almacenamos también
+        if (this.getCard() != null) {
+            result.put("card", this.getCard().toMap());
+        }
+        return result;
+    }
+
+    // Constructor para crear una celda a partir de un mapa
+    public Cell(Map<String, Object> map) {
+        // Recuperamos la fila y la columna de la celda
+        this.row = ((Long) map.get("row")).intValue();
+        this.col = ((Long) map.get("col")).intValue();
+        this.card = new ObservableField<>();
+        // Si hay una carta en el mapa, la recuperamos y la establecemos en la celda
+        if (map.containsKey("card")) {
+            this.card.set(new Card((Map<String, Object>) map.get("card")));
+        }
     }
 }
