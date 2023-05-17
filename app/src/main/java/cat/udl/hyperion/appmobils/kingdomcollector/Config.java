5be6578baa086a64243cd09d1848b207dacb3fb9
clausarlle <claudiasarlle@gmail.com>
2023-05-17 18:04:50 +0200

    About us ux game settings

---
+++ app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/Config.java
@@ -0,0 +1,14 @@
+package cat.udl.hyperion.appmobils.kingdomcollector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Config extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
