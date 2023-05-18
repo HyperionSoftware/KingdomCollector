6d9fcfd191ebbd3e1e2ef6f0e8b5d40e78d09067
clausarlle <claudiasarlle@gmail.com>
2023-05-18 15:52:51 +0200

    ux congig implement botton

--- app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/other/Config.java
+++ app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/other/Config.java
@@ -1,8 +1,9 @@
 package cat.udl.hyperion.appmobils.kingdomcollector.other;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import cat.udl.hyperion.appmobils.kingdomcollector.R;
 
@@ -12,5 +13,31 @@ public class Config extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+
+        // Button go back to main:
+        findViewById(R.id.button_back_from_config).setOnClickListener(view -> goToMainActivity());
+
+        // Button go about us
+        findViewById(R.id.button_about_us).setOnClickListener(view -> goToAboutus());
+
+        // Button terms and conditions
+        findViewById(R.id.button_terms_and_conditions).setOnClickListener(view -> goToTermsAndConditions());
+
     }
+
+    private void goToTermsAndConditions() {
+        Intent intent = new Intent(this, TermsAndConditions.class);
+        startActivity(intent);
+    }
+
+    private void goToAboutus() {
+        Intent intent = new Intent(this, AboutUs.class);
+        startActivity(intent);
+    }
+
+    private void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
