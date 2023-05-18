4694e1f7bcca473de9c7964941b88b8e64e48f29
clausarlle <claudiasarlle@gmail.com>
2023-05-18 19:21:32 +0200

    HOLA QUE COÑO PASA

---
+++ app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/choos_mode.java
@@ -0,0 +1,14 @@
+package cat.udl.hyperion.appmobils.kingdomcollector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class choos_mode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choos_mode);
+    }
+}
