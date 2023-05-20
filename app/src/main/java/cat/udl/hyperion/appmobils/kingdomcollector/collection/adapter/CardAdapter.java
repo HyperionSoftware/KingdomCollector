0ff7b64d498822e66129616a053470f33db21eb4
RickyBP <ricardbope@gmail.com>
2023-05-20 12:10:31 +0200

    Logcats

--- app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/collection/adapter/CardAdapter.java
+++ app/src/main/java/cat/udl/hyperion/appmobils/kingdomcollector/collection/adapter/CardAdapter.java
@@ -1,5 +1,6 @@
 package cat.udl.hyperion.appmobils.kingdomcollector.collection.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,27 +15,33 @@ import cat.udl.hyperion.appmobils.kingdomcollector.R;
 import cat.udl.hyperion.appmobils.kingdomcollector.game.models.Card;
 
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder> {
+    private static final String TAG = "CardAdapter";
     private List<Card> cards;
 
     public CardAdapter(List<Card> cards) {
         this.cards = cards;
+        Log.d(TAG, "CardAdapter iniciado con " + cards.size() + " cartas.");
     }
 
     @Override
     public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.v(TAG, "onCreateViewHolder llamado.");
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false);
         return new CardViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(CardViewHolder holder, int position) {
+        Log.v(TAG, "onBindViewHolder llamado para la posición " + position + ".");
         Card card = cards.get(position);
         holder.bind(card);
     }
 
     @Override
     public int getItemCount() {
-        return (cards != null ? cards.size() : 0);
+        int size = (cards != null ? cards.size() : 0);
+        Log.d(TAG, "getItemCount llamado, devolviendo " + size + "."); // registro de depuración
+        return size;
     }
 
 
@@ -48,9 +55,11 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
             cardImage = itemView.findViewById(R.id.card_image); // reemplaza con tus ids
             cardName = itemView.findViewById(R.id.card_name);
             cardType = itemView.findViewById(R.id.card_type);
+            Log.d(TAG, "CardViewHolder creado.");
         }
 
         public void bind(Card card) {
+            Log.i(TAG, "Vinculando carta: " + card.getName() + " de tipo: " + card.getType() + ".");
             cardImage.setImageResource(card.getImageResource());
             cardName.setText(card.getName());
             cardType.setText(card.getType());
